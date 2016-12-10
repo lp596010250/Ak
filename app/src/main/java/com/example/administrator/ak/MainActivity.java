bfebb94c8191d2dcf5ab06cadc7619409b5a3ea2
lp596010250 <596010250@qq.com>
2016-12-11 02:18:00 +0800

    我的okHttp3

--- app/src/main/java/com/example/administrator/ak/MainActivity.java
+++ app/src/main/java/com/example/administrator/ak/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.administrator.ak;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.administrator.ak.okHttpGet.OkHttpGetActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.btn_main);
+        btn1.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_main:
+                startActivity(new Intent(MainActivity.this, OkHttpGetActivity.class));
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/example/administrator/ak/okHttpGet/OkHttpGetActivity.java
@@ -0,0 +1,59 @@
+package com.example.administrator.ak.okHttpGet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.administrator.ak.R;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpGetActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ok_http_get);
+
+        button = (Button) findViewById(R.id.okhttp_btn11);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+        OkHttpClient client=new OkHttpClient();
+         Request request=new Request.Builder()
+
+                .url("https://www.baidu.com")
+                .build();
+
+
+         client.newCall(request).enqueue(new Callback() {
+             @Override
+             public void onFailure(Call call, IOException e) {
+
+             }
+
+             @Override
+             public void onResponse(Call call, Response response) throws IOException {
+                 Log.e("---", "onResponse: "+response.body().string()+"响应码：==="+response.code());
+             }
+         });
+
+
+            }
+        });
+    }
+}
